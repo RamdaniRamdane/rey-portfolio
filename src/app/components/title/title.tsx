90dadbb42999d6b0cd8a8d76a43ff6f7fc9d6ddd
RamdaniRamdane <ramdaniramdane123+1@gmail.com>
2024-08-28 19:03:05 +0100

    handle type

--- src/app/components/title/title.tsx
+++ src/app/components/title/title.tsx
@@ -1,11 +1,19 @@
-import styles from './title.module.css'
+import styles from './title.module.css';
 
-function Title(props){
-    return(
-        <div>
-            <h1 className={styles.text}>{props.text} <span className={styles.textColored}>{props.textColored}</span></h1>
-        </div>
-    )
+interface TitleProps {
+    text: string;
+    textColored: string;
 }
 
-export default Title 
+function Title({ text, textColored }: TitleProps) {
+    return (
+        <div>
+            <h1 className={styles.text}>
+                {text} <span className={styles.textColored}>{textColored}</span>
+            </h1>
+        </div>
+    );
+}
+
+export default Title;
+
